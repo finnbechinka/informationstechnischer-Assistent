4aa894743c7d9a83523792c88d3655803615d9e0
Finn <26581865+shakerrrr@users.noreply.github.com>
2020-02-01 23:56:30 +0100

    Create main.c

---
+++ c/mooseemooseemo-kopf/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x=1;
+    while(x<3){
+        printf("Moo");
+        printf("See");
+        x=x+1;
+    }
+    if (x==3){
+        printf("Mo");
+    }
+    return 0;
+}
