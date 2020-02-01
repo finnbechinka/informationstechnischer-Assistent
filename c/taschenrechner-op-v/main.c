12e877b27d75abc100643ac7a6b129f75ad10582
Finn <26581865+shakerrrr@users.noreply.github.com>
2020-02-02 00:30:06 +0100

    Create main.c

---
+++ c/taschenrechner-op-v/main.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int programmvariable=1;
+do {
+
+    int zahl1;
+    int zahl2;
+    int ergebnis;
+    char operation;
+
+    printf("Bitte geben Sie eine Zahl ein: ");
+    scanf("%d", &zahl1);
+
+    printf("Bitte geben Sie einen Operator ein: ");
+    scanf(" %c",&operation);
+
+    printf("Bitte geben Sie eine weitere Zahl ein: ");
+    scanf("%d", &zahl2);
+
+
+
+switch( operation )
+  {
+  case '+':
+    ergebnis=zahl1+zahl2;
+    break;
+  case '-':
+    ergebnis=zahl1-zahl2;
+    break;
+  case '*':
+    ergebnis=zahl1*zahl2;
+    break;
+  case '/':
+    ergebnis=zahl1/zahl2;
+    break;
+  default:
+    fprintf(stderr, "Ungueltiger Operator! \n");
+    goto restart;
+  }
+
+       if (ergebnis==zahl1+zahl2){
+
+       printf("----------------------------------------\n");
+       printf("Summe von %d%c%d                  %d\n", zahl1, operation, zahl2, ergebnis);
+       printf("----------------------------------------\n");
+
+    }else {
+
+      if (ergebnis==zahl1-zahl2) {
+        printf("----------------------------------------\n");
+        printf("Differenz von %d%c%d                  %d\n", zahl1, operation, zahl2, ergebnis);
+        printf("----------------------------------------\n");
+
+    }else {
+      if (ergebnis==zahl1*zahl2) {
+        printf("----------------------------------------\n");
+        printf("Produkt von %d%c%d                  %d\n", zahl1, operation, zahl2, ergebnis);
+        printf("----------------------------------------\n");
+
+    }else {
+      if (ergebnis==zahl1/zahl2) {
+        printf("----------------------------------------\n");
+        printf("Quotient von %d%c%d                  %d\n", zahl1, operation, zahl2, ergebnis);
+        printf("----------------------------------------\n");
+      }
+    }
+    }
+    }
+
+
+restart: printf("Moechten Sie eine neue Rechnung durchfueren? (1/0)");
+scanf("%i", &programmvariable);
+
+if (programmvariable>1) {
+    programmvariable=1;
+    printf("Bitte geben Sie nur 0 (um dem Prozess zu beenden) oder 1 (um eine neue Rechnung durchzufuehren) ein.\n");
+    goto restart;
+  }
+
+if (programmvariable<0) {
+    programmvariable=1;
+    printf("Bitte geben Sie nur 0 (um dem Prozess zu beenden) oder 1 (um eine neue Rechnung durchzufuehren) ein.\n");
+    goto restart;
+}
+
+}
+
+while(programmvariable==1);
+
+
+    return 0;
+}
